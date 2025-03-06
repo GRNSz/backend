7c9e866852128b5286072f3d7d8caffd0245a25d
GRNSz <gugzribeiro@gmail.com>
2025-03-06 13:54:14 -0300

    Subindo algumas correções

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
 
+Route::get('/teste', function () {
+    return Inertia::render('teste');
+})->name('teste')-> middleware('web');
+
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
